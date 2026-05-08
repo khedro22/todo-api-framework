d6419cfa03bc6114ba0da90c2544b5b014d773fa
Mohamed Khedr <mkhedr652@gmail.com>
2026-05-08 11:02:33 +0300

    Defining User Pojo class with attributes

--- src/test/java/com/qacart/todo/models/User.java
+++ src/test/java/com/qacart/todo/models/User.java
@@ -1,4 +1,66 @@
 package com.qacart.todo.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+@JsonInclude(JsonInclude.Include.NON_NULL) //To make null values accepted (this POJO is used for both request and response)
 public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @JsonProperty("access_token")
+    private String accessToken;
+    private String userID;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    @JsonProperty("access_token")
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    @JsonProperty("access_token")
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
 }
